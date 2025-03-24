f2599d27de1f113d06f4733940ee8896cb745321
Prashant Singh <130375284+Pr4shantHere@users.noreply.github.com>
2025-03-24 10:41:48 +0530

    final commit, it was fun

--- AudioPlayer.gd
+++ AudioPlayer.gd
@@ -3,14 +3,19 @@ extends AudioStreamPlayer
 const music = preload("res://audios/music3.mp3")
 const cm = preload("res://audios/credit song.mp3")
 var useGameMusic = true
-
+var vol : int = 0
 func _play_music(music : AudioStream, volume = 0):
 	if stream == music:
 		return
 	stream = music
-	volume_db = volume
 	play()
 	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("UP"):
+		vol += 1
+	elif Input.is_action_just_pressed("DOWN"):
+		vol -= 1
+	volume_db = vol
 	
 func play_music_level():
 	if useGameMusic:
